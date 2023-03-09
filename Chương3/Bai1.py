9e38b4749d20c561e279fc8ae0f9118dc250fe3b
leduyenduyen3004123 <leduyenduyen3004123@gmail.com>
2023-03-09 10:27:44 +0700

    push the code tho github

---
+++ Chương3/Bai1.py
@@ -0,0 +1,15 @@
+a=float(input('a='))
+b=float(input('b='))
+c=float(input('c='))
+x=a
+if b>x:
+    x=b
+if c>x:
+    x=c
+print('SLN=',x)
+y=b
+if a<y:
+    y=a
+if c<y:
+    y=c
+print ('SBN=', y)
